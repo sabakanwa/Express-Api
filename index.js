9d98cd83c5699994b60504be0e0f1f3e0abadf76
saba parveen <sabakanwal5566@gmail.com>
2023-08-01 20:20:47 +0500

    FEAT:Brands Api Add

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const CategoryRouter = require('./Api/Category/Router')
-
+const BrandRouter = require('./Api/Brand/Router')
 const port = process.env.SERVER_PORT || 3200
 
 app.use(express.json())
 app.use('/api', CategoryRouter)
+app.use('/api' ,BrandRouter)
 
 
 
