386a7b77438211f54b2959bfda470d2ebbf33340
saba parveen <sabakanwal5566@gmail.com>
2023-08-01 16:28:37 +0500

    CreateCategory-Post Api

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const CategoryRouter = require('./Api/Category/Router')
+
+const port = process.env.SERVER_PORT || 3200
+
+app.use(express.json())
+app.use('/api', CategoryRouter)
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
